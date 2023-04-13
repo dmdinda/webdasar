adf8a2966deba3a9ef033283e1e4f540e2da6fab
dmdinda <mulyasaridinda@gmail.com>
2023-04-13 14:41:23 +0700

    percobaan php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $nama_kamu = "Dinda Mulyasari";
+    $umur_sekarang = 21;
+    // tanda petik dua
+    /*
+    echo "Hello World, Welcome ".$nama_kamu;
+    echo " umur kamu ".$umur_sekarang;
+    echo " tahun"
+    echo "<br/>";
+    */
+    echo "Hello World, Welcome ".$nama_kamu." umur kamu ".$umur_sekarang." tahun";
+    echo "<br/>";
+    echo "berarti $nama_kamu lahir di tahun ";
+    echo 2023-$umur_sekarang;
+
+    // tanda petik satu
+    // echo 'hello world';
+
+?>
